c9f4323085e42098d1ed5489913279b1cf54a01c
Przemysław Kamiński <sshembou@gmail.com>
2025-01-14 18:35:49 +0100

    chore: pushed additional enumerated-tiles-section changes

--- themes/blocks/enumerated-tiles-section/build/enumerated-tiles-section/index.asset.php
+++ themes/blocks/enumerated-tiles-section/build/enumerated-tiles-section/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '7e3c987cdcf6850c3fc2');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'fa18b7379b08b848daa7');
--- themes/blocks/enumerated-tiles-section/src/enumerated-tiles-section/edit.js
+++ themes/blocks/enumerated-tiles-section/src/enumerated-tiles-section/edit.js
@@ -13,7 +13,7 @@ import { __ } from '@wordpress/i18n';
  */
 import { useBlockProps, InspectorControls, MediaUpload } from '@wordpress/block-editor';
 import { PanelBody, PanelRow, Button, TextControl, IconButton, TextareaControl } from '@wordpress/components';
-
+import CustomButton from '../../../common/CustomButton'
 /**
  * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
  * Those files can contain any CSS code that gets applied to the editor.
@@ -200,6 +200,12 @@ export default function Edit({ attributes, setAttributes }) {
 						</>
 					))}
 				</div>
+				<CustomButton {...button}>
+					<>
+						{button.text}
+						<img src={button.svg} />
+					</>
+				</CustomButton>
 			</section>
 		</>
 	);
