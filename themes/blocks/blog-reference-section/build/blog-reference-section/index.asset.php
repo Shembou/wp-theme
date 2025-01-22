561bc6071593fa3752456163a29db6133d105ad1
Przemyslaw Kaminski <sshembou@gmail.com>
2025-01-22 22:17:54 +0100

    chore: changed icon of blog-reference-section

--- themes/blocks/blog-reference-section/build/blog-reference-section/index.asset.php
+++ themes/blocks/blog-reference-section/build/blog-reference-section/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '35affb9832f1bf08d99c');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '0de01ccb9103a66c79ad');
