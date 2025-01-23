696b74318951fc08341964801df06a5664ece84a
Przemyslaw Kaminski <sshembou@gmail.com>
2025-01-23 18:01:10 +0100

    fix: fixed save.js

--- themes/blocks/detailed-employees-section/build/detailed-employees-section/index.asset.php
+++ themes/blocks/detailed-employees-section/build/detailed-employees-section/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'edb83b3014f04dff0398');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '0541ed42ea5a3ebaf99a');
