518a6197065f75f41bff578bf1296725d0c8d87c
Przemyslaw Kaminski <sshembou@gmail.com>
2025-01-28 16:57:03 +0100

    fix: minor fixes

--- themes/blocks/tags-section/build/tags-section/index.asset.php
+++ themes/blocks/tags-section/build/tags-section/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'ba70956c26615748ce6c');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'a26db7e4d00659e22cc2');
--- themes/blocks/tags-section/src/tags-section/save.js
+++ themes/blocks/tags-section/src/tags-section/save.js
@@ -19,24 +19,39 @@ export default function save({ attributes }) {
 	const {
 		tags,
 		tag,
-		heading,
+		heading = '',
 		paragraph
 	} = attributes
-	return (
-		<section {...useBlockProps.save()} id='tags-section'>
-			{heading != '' && <header>
-				<p className='tag'>{tag}</p>
-				<h2>{heading}</h2>
-				<p>{paragraph}</p>
-			</header>
-			}
-			<div className={`tags ${heading != '' ? 'heading-variant' : ''}`}>
-				{tags && tags.map(({ tag, url }, index) => (
-					<a className='tag' key={index} href={url}>
-						{tag}
-					</a>
-				))}
-			</div>
-		</section>
-	);
+
+	if (heading != '') {
+		return (
+			<section {...useBlockProps.save()} id='tags-section'>
+				{heading != '' && <header>
+					<p className='tag'>{tag}</p>
+					<h2>{heading}</h2>
+					<p>{paragraph}</p>
+				</header>
+				}
+				<div className={`tags ${heading != '' ? 'heading-variant' : ''}`}>
+					{tags && tags.map(({ tag, url }, index) => (
+						<a className='tag' key={index} href={url}>
+							{tag}
+						</a>
+					))}
+				</div>
+			</section>
+		)
+	} else {
+		return (
+			<section {...useBlockProps.save()} id='tags-section'>
+				<div className={`tags `}>
+					{tags && tags.map(({ tag, url }, index) => (
+						<a className='tag' key={index} href={url}>
+							{tag}
+						</a>
+					))}
+				</div>
+			</section>
+		)
+	}
 }
